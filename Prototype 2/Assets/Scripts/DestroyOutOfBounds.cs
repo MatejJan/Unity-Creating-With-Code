e4ab5d1d72dd6174cefa8187f3138a7fa6acbd32
Matej Jan <matej@retronator.com>
2020-12-08 16:37:57 +0100

    Completed lessons 2.1, 2.2.

---
+++ Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    private float topBound = 30;
+    private float bottomBound = -10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float z = transform.position.z;
+
+        if (z < bottomBound || z > topBound)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 10;
+    public float xRange = 10;
+
+    public GameObject projectilePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        Vector3 newPosition = transform.position;
+
+        newPosition.x += speed * Time.deltaTime * horizontalInput;
+        newPosition.x = Mathf.Clamp(newPosition.x, -xRange, xRange);
+        transform.position = newPosition;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+    }
+}
