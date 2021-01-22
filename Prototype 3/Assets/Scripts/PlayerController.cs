4aba646f3405de29cbd07e0f995cced244cbbacf
Matej Jan <matej@retronator.com>
2021-01-22 15:34:44 +0100

    Project and challenge 3 completed.

---
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float jumpForce = 10;
+    public float gravityMultiplier = 1;
+    public bool isOnGround = true;
+    public bool gameOver = false;
+    public ParticleSystem explosionParticleSystem;
+    public ParticleSystem dirtSplatterParticleSystem;
+    public AudioClip jumpAudioClip;
+    public AudioClip crashAudioClip;
+
+    private new Rigidbody rigidbody;
+    private Animator animator;
+    private AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+
+        Physics.gravity *= gravityMultiplier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
+        {
+            rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+            animator.SetTrigger("Jump_trig");
+            dirtSplatterParticleSystem.Stop();
+            audioSource.PlayOneShot(jumpAudioClip);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+            dirtSplatterParticleSystem.Play();
+        }
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            gameOver = true;
+            animator.SetBool("Death_b", true);
+            animator.SetInteger("DeathType_int", 1);
+            explosionParticleSystem.Play();
+            dirtSplatterParticleSystem.Stop();
+            audioSource.PlayOneShot(crashAudioClip);
+        }
+    }
+}
---
+++ Prototype 3/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+
+    private Vector3 spawnPosition = new Vector3(25, 0, 0);
+    private float startTime = 2;
+    private float repeatRate = 2;
+    private PlayerController playerController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacle", startTime, repeatRate);
+        playerController = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void SpawnObstacle()
+    {
+        if (!playerController.gameOver)
+        {
+            Instantiate(obstaclePrefab, spawnPosition, obstaclePrefab.transform.rotation);
+        }
+    }
+}
