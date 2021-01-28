d43ef9b9e42a57d5a78ac0135e69d7cea6b51022
Matej Jan <matej@retronator.com>
2021-01-28 19:17:30 +0100

    Added Prototype 4.

---
+++ Prototype 4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5;
+
+    private Rigidbody rigidbody;
+    private GameObject focalPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        rigidbody.AddForce(focalPoint.transform.forward * speed * forwardInput);
+    }
+}
