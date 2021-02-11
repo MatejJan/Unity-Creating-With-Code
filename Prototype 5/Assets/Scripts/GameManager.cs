e914355653f56548cdc8c9009e267af4d352743d
Matej Jan <matej@retronator.com>
2021-02-11 09:09:30 +0100

    Gameplay done for prototype 5.

---
+++ Prototype 5/Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    public TextMeshProUGUI scoreText;
+
+    private float spawnGapDuration = 1;
+    private int score = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateScore(0);
+
+        StartCoroutine(SpawnTarget());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private IEnumerator SpawnTarget()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnGapDuration);
+
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+
+    public void UpdateScore(int scoreChange)
+    {
+        score += scoreChange;
+        scoreText.text = $"Score: {score}";
+    }
+}
---
+++ Prototype 5/Assets/Scripts/Target.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public ParticleSystem explosionParticleSystem;
+
+    public int scoreValue;
+
+    private GameManager gameManager;
+
+    private Rigidbody rigidbody;
+
+    private float minSpeed = 13.5f;
+    private float maxSpeed = 18f;
+    private float maxTorque = 10;
+    private float xRange = 4;
+    private float ySpawnPos = -6;
+
+    void Start()
+    {
+        gameManager = Object.FindObjectOfType<GameManager>();
+
+        rigidbody = GetComponent<Rigidbody>();
+
+        rigidbody.AddForce(RandomForce(), ForceMode.Impulse);
+        rigidbody.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+
+        transform.position = RandomSpawnPos();
+    }
+
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+
+    float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+
+    Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+        Instantiate(explosionParticleSystem, transform.position, Quaternion.identity);
+
+        gameManager.UpdateScore(scoreValue);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
